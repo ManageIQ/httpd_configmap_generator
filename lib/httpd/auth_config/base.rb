ccc1f312f2b48b4e1ed8eaef2b404e0811cc03c0
Alberto Bellotti <abellott@redhat.com>
2017-09-27 16:03:36 -0400

    bundle install would fail with require active_support/core_ext/string

--- lib/httpd/auth_config/base.rb
+++ lib/httpd/auth_config/base.rb
@@ -7,6 +7,8 @@ module Httpd
     private
 
     def self.auth_class(auth_type)
+      require "active_support/core_ext/string" # for camelize
+
       auth_type = auth_type.camelize
       raise "Invalid Authentication Type #{auth_type} specified" unless const_defined?(auth_type, false)
       const_get(auth_type, false)
