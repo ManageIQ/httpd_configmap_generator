0f4b38945ea14692ac46d19c4de0d7193c43daf0
Alberto Bellotti <abellott@redhat.com>
2017-10-03 10:03:47 -0400

    Updated configmap generation to build the hash equivalent then using .to_yaml.

--- lib/httpd_auth_config/base/configmap.rb
+++ lib/httpd_auth_config/base/configmap.rb
@@ -1,82 +1,66 @@
 require "base64"
+require "yaml"
 
 module HttpdAuthConfig
   class Base
     def generate_configmap(auth_type, auth_configuration, realm, file_list)
-      configmap_header(auth_type, auth_configuration, realm)
-      configmap_file_section(file_list)
-      configmap_trailer
+      config_map = auth_configmap_template(auth_type, auth_configuration, realm)
+      file_specs = gen_filespecs(file_list)
+      configmap_configuration(config_map, file_specs)
+      configmap_file_list(config_map, file_specs)
+      puts config_map.to_yaml
     end
 
     private
 
-    def out(section, line)
-      prefix = ""
-      if section == 1
-        prefix = "  "
-      elsif section == 2
-        prefix = "    "
-      elsif section == 3
-        prefix = "      "
-      end
-      puts "#{prefix}#{line}\n"
+    def auth_configmap_template(auth_type, auth_configuration, kerberos_realms)
+      {
+        "data"     => {
+          "auth-type"            => auth_type,
+          "auth-configuration"   => auth_configuration,
+          "auth-kerberos-realms" => kerberos_realms
+        },
+        "kind"     => "ConfigMap",
+        "metadata" => {
+          "name" => "httpd-auth-configs"
+        }
+      }
     end
 
-    def configmap_basename(file_spec)
-      basename = file_spec[:basename]
-      file_spec[:binary] ? "#{basename}.base64" : basename
-    end
-
-    def configmap_header(auth_type, auth_configuration, kerberos_realms)
-      out(0, "data:")
-      out(1, "auth-type: #{auth_type}")
-      out(1, "auth-configuration: #{auth_configuration}")
-      out(1, "auth-kerberos-realms: #{kerberos_realms}")
-    end
-
-    def configmap_trailer
-      out(0, "kind: ConfigMap")
-      out(0, "metadata:")
-      out(1, "name: httpd-auth-configs")
-    end
-
-    def configmap_configuration(file_specs)
-      out(1, "auth-configuration.conf: |")
-      out(2, "# External Authentication Configuration File")
-      out(2, "#")
-      file_specs.each do |file_spec|
-        out(2, "file = #{configmap_basename(file_spec)} #{file_spec[:target]} #{file_spec[:mode]}")
-      end
-    end
-
-    def configmap_file_list(file_specs)
-      file_specs.each do |file_spec|
-        out(1, "#{configmap_basename(file_spec)}: |")
-        if file_spec[:binary]
-          Base64.encode64(File.read(file_spec[:target])).split("\n").each do |line|
-            out(3, line)
-          end
-        else
-          File.read(file_spec[:target]).split("\n").each do |line|
-            out(3, line)
-          end
-        end
-      end
-    end
-
-    def configmap_file_section(file_list)
+    def gen_filespecs(file_list)
       file_specs = []
       file_list.each do |file|
         file = file.strip
-        basename = File.basename(file).dup
-        binary = file_binary?(file)
         stat = File.stat(file)
-        mode = sprintf("%4o:%s:%s", stat.mode & 07777, stat.uid, stat.gid)
-        file_specs << { :basename => basename, :binary => binary, :target => file, :mode => mode }
+        file_specs << {
+          :basename => File.basename(file).dup,
+          :binary   => file_binary?(file),
+          :target   => file,
+          :mode     => "%4o:%s:%s" % [stat.mode & 0o7777, stat.uid, stat.gid]
+        }
       end
-      file_specs = file_specs.sort_by { |file_spec| file_spec[:basename] }
-      configmap_configuration(file_specs)
-      configmap_file_list(file_specs)
+      file_specs.sort_by { |file_spec| file_spec[:basename] }
+    end
+
+    def configmap_configuration(config_map, file_specs)
+      auth_configuration = "# External Authentication Configuration File\n#\n"
+      file_specs.each do |file_spec|
+        auth_configuration += "file = #{configmap_basename(file_spec)} #{file_spec[:target]} #{file_spec[:mode]}\n"
+      end
+      config_map["data"].merge!("auth-configuration.conf" => auth_configuration)
+    end
+
+    def configmap_file_list(config_map, file_specs)
+      file_specs.each do |file_spec|
+        content = File.read(file_spec[:target])
+        content = Base64.encode64(content) if file_spec[:binary]
+        # encode(:universal_newline => true) will convert \r\n to \n, necessary for to_yaml to render properly.
+        config_map["data"].merge!(configmap_basename(file_spec) => content.encode(:universal_newline => true))
+      end
+    end
+
+    def configmap_basename(file_spec)
+      file_spec[:binary] ? "#{file_spec[:basename]}.base64" : file_spec[:basename]
     end
   end
 end
